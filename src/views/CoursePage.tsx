d4d395fe6a58131cc8bb7224c0811cc5f75ba568
serh11p <git@serh11p.com>
2020-11-11 13:10:22 +0200

    Improve Content-Code layout

--- src/views/CoursePage.tsx
+++ src/views/CoursePage.tsx
@@ -15,24 +15,26 @@ interface CoursePageProps {
 enum TranspilationStatus {
   transpiling = "Transpiling...",
   success = "Successfully transpiled Solidity to Ligo",
-  error = "Error occured during transpilation"
+  error = "Error occured during transpilation",
 }
 
 const CoursePage: React.FC<CoursePageProps> = ({ selectedSection }) => {
   const section = sections[selectedSection];
   const [solidityCode, setSolidityCode] = useState(section.solidity);
   const [ligoCode, setLigoCode] = useState(section.ligo);
-  const [transpilationStatus, setTranspilationStatus] = useState<TranspilationStatus>(TranspilationStatus.success);
+  const [transpilationStatus, setTranspilationStatus] = useState<
+    TranspilationStatus
+  >(TranspilationStatus.success);
 
   useEffect(() => {
     setSolidityCode(section.solidity);
-    setLigoCode(section.ligo)
+    setLigoCode(section.ligo);
   }, [section]);
 
   return (
     <Layout selectedSection={selectedSection}>
       {/* <div className="flex flex-row max-w-full w-full h-full no-wrap"> */}
-      <div className="flex flex-col w-full max-w-50 h-full">
+      <div className="flex flex-col w-1/2 h-full">
         <article className="flex-1 w-full py-4 px-6 bg-white prose lg:prose-md max-w-full overflow-y-auto break-normal">
           <h1>
             {selectedSection + 1}. {section.title}
@@ -63,7 +65,7 @@ const CoursePage: React.FC<CoursePageProps> = ({ selectedSection }) => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-col w-full max-w-50 h-full border-l-2 border-gray-100">
+      <div className="flex flex-col w-1/2 h-full border-l-2 border-gray-100">
         <MonacoEditor
           language="sol"
           theme="vs-dark"
@@ -80,7 +82,7 @@ const CoursePage: React.FC<CoursePageProps> = ({ selectedSection }) => {
             if (window && (window as any).compile) {
               try {
                 const result = await (window as any).compile(code);
-                setLigoCode(result.result)
+                setLigoCode(result.result);
                 setTranspilationStatus(TranspilationStatus.success);
               } catch (e) {
                 setTranspilationStatus(TranspilationStatus.error);
@@ -89,17 +91,26 @@ const CoursePage: React.FC<CoursePageProps> = ({ selectedSection }) => {
           }}
           editorDidMount={(_, editor) => console.log(editor)}
         />
-        <div className={clsx("h-10 w-full px-6 font-bold text-white text-center text-sm", {
-          "bg-green-500": transpilationStatus === TranspilationStatus.success,
-          "bg-indigo-500": transpilationStatus === TranspilationStatus.transpiling,
-          "bg-red-500": transpilationStatus === TranspilationStatus.error
-        })}>↓ {transpilationStatus}</div>
+        <div
+          className={clsx(
+            "h-10 w-full px-6 font-bold text-white text-center text-sm",
+            {
+              "bg-green-500":
+                transpilationStatus === TranspilationStatus.success,
+              "bg-indigo-500":
+                transpilationStatus === TranspilationStatus.transpiling,
+              "bg-red-500": transpilationStatus === TranspilationStatus.error,
+            }
+          )}
+        >
+          ↓ {transpilationStatus}
+        </div>
         <MonacoEditor
           language="pascaligo"
           theme="vs-dark"
           value={ligoCode}
           options={{
-            ariaLabel: 'Solidity',
+            ariaLabel: "Solidity",
             fontSize: 14,
             minimap: {
               enabled: false,
