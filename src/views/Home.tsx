4c0fe0d2bddd7bbcd30379855927429079ad4741
Korney Vasilchenko <starkkor@gmail.com>
2021-01-22 15:41:33 +0200

    Add proper sol2ligo link

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -40,7 +40,7 @@ export default function HomePage() {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href="#"
+                    href="https://madfish-solutions.github.io/sol2ligo/"
                     className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Try sol2ligo
