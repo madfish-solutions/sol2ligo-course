b8a72922640e5fdf33bf66b2319ee4264d5cdd44
Korney Vasilchenko <starkkor@gmail.com>
2021-02-12 17:49:24 +0200

    Fix the link

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -41,12 +41,12 @@ export default function HomePage() {
                   </Link>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link
-                    to="https://madfish-solutions.github.io/sol2ligo/"
+                  <a
+                    href="https://madfish-solutions.github.io/sol2ligo/"
                     className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Try sol2ligo
-                  </Link>
+                  </a>
                 </div>
               </div>
             </div>
